ec85abef9deb257dd6427e4052fec8b60c797ddb
uyaseen <uyaseen.26@gmail.com>
2016-03-12 17:01:23 +0100

    gradient clipping added to mitigate exploding gradients

--- utilities/optimizers.py
+++ utilities/optimizers.py
@@ -5,6 +5,8 @@ import numpy as np
 
 __author__ = 'uyaseen'
 
+grad_clip = 5
+
 
 # source: https://gist.github.com/Newmu/acb738767acb4788bac3
 def adam(cost, params, lr=0.001, b1=0.9, b2=0.999, e=1e-8):
@@ -16,6 +18,7 @@ def adam(cost, params, lr=0.001, b1=0.9, b2=0.999, e=1e-8):
     fix2 = 1. - (1. - b2)**i_t
     lr_t = lr * (T.sqrt(fix2) / fix1)
     for p, g in zip(params, grads):
+        T.clip(g, -grad_clip, grad_clip)
         m = theano.shared(p.get_value() * 0.)
         v = theano.shared(p.get_value() * 0.)
         m_t = (b1 * g) + ((1. - b1) * m)
@@ -33,6 +36,7 @@ def rmsprop(cost, params, lr=0.001, rho=0.9, epsilon=1e-6):
     grads = T.grad(cost=cost, wrt=params)
     updates = []
     for p, g in zip(params, grads):
+        T.clip(g, -grad_clip, grad_clip)
         acc = theano.shared(p.get_value() * 0.)
         acc_new = rho * acc + (1 - rho) * g ** 2
         gradient_scaling = T.sqrt(acc_new + epsilon)
@@ -44,7 +48,7 @@ def rmsprop(cost, params, lr=0.001, rho=0.9, epsilon=1e-6):
 
 def sgd_vanilla(cost, params, lr=0.001):
     grads = T.grad(cost, params)
-    updates = [(param, param - lr * grad)
+    updates = [(param, param - lr * T.clip(grad, -grad_clip, grad_clip))
                for param, grad in zip(params, grads)
                ]
     return updates
